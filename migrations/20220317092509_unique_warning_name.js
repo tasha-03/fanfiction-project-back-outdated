8742e088187b3cf54b3f6df3f4991797dc5000cb
tasha-03 <tatyana.pavl03@gmail.com>
2022-03-25 13:29:43 +0300

    Unique warning name

---
+++ migrations/20220317092509_unique_warning_name.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  await knex.schema.alterTable("warnings", (table) => {
+    table.string("name").unique().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.alterTable("warnings", (table) => {
+    table.dropUnique("name");
+  });
+};
