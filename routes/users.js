2bcfe65841719faab579d6b95c00f4afe3514a6c
tasha-03 <tatyana.pavl03@gmail.com>
2022-04-27 22:36:37 +0300

    Added request data validation

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,32 @@ const usersController = require("../controllers/users");
 const auth = require("./middlewares/auth");
 const { sign: signToken } = require("../utils/token");
 const { wrap } = require("async-middleware");
-// const {validate} = require("./middlewares/validate");
+const validate = require("./middlewares/validate");
+const { body, param, query } = require("express-validator");
+const { TZs } = require("./middlewares/TZs");
 
 const router = express.Router();
 
 router.post(
   "/signup",
+  body("login")
+    .trim()
+    .matches(/^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d.\-_]{7,}$/)
+    .withMessage(
+      "Your login must contain at least 8 symbols, start with a letter, end with a letter or number, contain letters, numbers, hyphen or underscore, and must not contain spaces, special characters, or emoji."
+    ),
+  body("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Please provide proper email address."),
+  body("password")
+    .trim()
+    .isStrongPassword()
+    .withMessage(
+      "The password must contain at least 8 symbols, have at least one number, one uppercase letter, one lowercase letter and one special symbol."
+    ),
+  validate(),
   wrap(async (req, res) => {
     const { login, email, password } = req.body;
     const { userId } = await usersController.register({
@@ -32,6 +52,11 @@ router.get(
 
 router.post(
   "/email/confirm",
+  body("confirmationCode")
+    .trim()
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Confirmation code must consist of exactly 6 symbols."),
+  validate(),
   auth("USER"),
   wrap(async (req, res) => {
     const { confirmationCode } = req.body;
@@ -45,6 +70,9 @@ router.post(
 
 router.post(
   "/login",
+  body("login").trim(),
+  body("password").trim(),
+  validate(),
   wrap(async (req, res) => {
     const { login, password } = req.body;
     const { userId } = await usersController.login({ login, password });
@@ -55,18 +83,58 @@ router.post(
 
 router.get(
   "/myself",
+  query("tz").isIn(TZs),
+  validate(),
   auth("USER"),
   wrap(async (req, res) => {
-    // const user = await usersController.getUserById({
-    //   userId: req.user.userId,
-    //   tz: req.query.tz,
-    // });
     res.send({ success: true, user: req.user });
+  })
+);
+
+router.post(
+  "/",
+  body("login")
+    .trim()
+    .isString()
+    .customSanitizer((value) => {
+      value = value.replace("?", "\\?");
+      return value.replace("_", "\\_");
+    }),
+  query("limit").optional().trim().isInt(),
+  query("page").optional().trim().isInt(),
+  validate(),
+  wrap(async (req, res) => {
+    const users = await usersController.getUsersByLogin({
+      login: req.body.login,
+      limit: req.query.limit,
+      page: req.query.page,
+    });
+    res.send({ success: true, users });
+  })
+);
+
+router.get(
+  "/deactivate",
+  auth("USER"),
+  wrap(async (req, res) => {
+    await usersController.deactivateProfile({ userId: req.user.userId });
+    res.send({ success: true });
+  })
+);
+
+router.get(
+  "/activate",
+  auth("USER"),
+  wrap(async (req, res) => {
+    await usersController.activateProfile({ userId: req.user.userId });
+    res.send({ success: true });
   })
 );
 
 router.get(
   "/:id",
+  param("id").isInt(),
+  validate(),
   wrap(async (req, res) => {
     const user = await usersController.getUserById({
       userId: req.params.id,
@@ -76,26 +144,16 @@ router.get(
   })
 );
 
-router.get(
-  "/",
-  wrap(async (req, res) => {
-    const users = await usersController.getAllUsers({
-      limit: req.query.limit,
-      page: req.query.page,
-    });
-    res.send({ success: true, users });
-  })
-);
-
 router.post(
-  "/",
+  "/password/restore",
+  auth("USER"),
   wrap(async (req, res) => {
-    const users = await usersController.getUsersByLogin({
-      login: req.body.login,
-      limit: req.query.limit,
-      page: req.query.page,
+    await usersController.restorePassword({
+      userId: req.user.userId,
+      password: req.body.password,
+      passRestored: true,
     });
-    res.send({ success: true, users });
+    res.send({ success: true });
   })
 );
 
